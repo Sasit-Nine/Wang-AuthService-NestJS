f424193831934917bf8480c6ca7eb6b09366b5d8
6710110400 SASIT SUANSADANG <155415427+Sasit-Nine@users.noreply.github.com>
2025-04-23 16:47:12 +0700

    Documentation API Successfull

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -13,13 +13,23 @@ import { AuthGuard } from '@nestjs/passport';
 import { AuthService } from './auth/auth.service';
 import { JwtAuthGuard } from './auth/jwt-auth.guard';
 import { SignupDto } from './auth/signup.dto';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiTags, ApiBody, ApiParam } from '@nestjs/swagger';
 import { ApiBearerAuth } from '@nestjs/swagger';
 
 @Controller()
 export class AppController {
   constructor(private authService: AuthService) {}
   @UseGuards(AuthGuard('local'))
+  @ApiBody({
+    description: 'Login with username and password',
+    schema: {
+      type: 'object',
+      properties: {
+        username: { type: 'string', example: 'jane_smith' },
+        password: { type: 'string', example: 'securePass456' },
+      },
+    },
+  })
   @Post('auth/login')
   login(@Request() req) {
     // eslint-disable-next-line @typescript-eslint/no-unsafe-return, @typescript-eslint/no-unsafe-member-access
@@ -43,6 +53,18 @@ export class AppController {
 
   @ApiTags('Refresh Token')
   @Post('auth/refresh')
+  @ApiBody({
+    schema: {
+      type: 'object',
+      properties: {
+        refreshToken: {
+          type: 'string',
+          example:
+            'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjo4LCJ1c2VybmFtZSI6ImphbmVfc21pdGgiLCJlbXBfY29kZSI6IkVNUDAwOSIsInVzZXJfY3JlYXRlZCI6IjIwMjUtMDQtMTZUMDM6MjE6MjUuNzIwWiIsInR5cGUiOiJyZWZyZXNoIiwiaWF0IjoxNzQ1MzkzNDExLCJleHAiOjE3NDU0Nzk4MTF9.ymw5DPkFHHYcG9l1kY95nu35gAch-bmbA8jJAgASbkg',
+        },
+      },
+    },
+  })
   refresh(@Body('refreshToken') refreshToken: string) {
     if (!refreshToken)
       throw new BadRequestException('Refresh token is required');
@@ -50,6 +72,14 @@ export class AppController {
   }
 
   @ApiTags('Delete Employee Users by ID')
+  @UseGuards(JwtAuthGuard)
+  @ApiParam({
+    name: 'id',
+    required: true,
+    type: Number,
+    example: 5,
+    description: 'รหัสผู้ใช้ (user ID) ที่ต้องการลบ',
+  })
   @Delete('auth/delete-user/:id')
   async deleteUser(@Param('id') userId: number) {
     try {
